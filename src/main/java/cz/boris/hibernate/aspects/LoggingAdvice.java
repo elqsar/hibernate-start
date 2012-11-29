3c3f8eb4296d013cdef37aa3cf41f8db087009bd
Boris <boris.musatov@gmail.com>
2012-11-29 20:24:04 +0100

    logger set up and logging apsect added

--- src/main/java/cz/boris/hibernate/aspects/LoggingAdvice.java
+++ src/main/java/cz/boris/hibernate/aspects/LoggingAdvice.java
@@ -1,5 +1,37 @@
 package cz.boris.hibernate.aspects;
 
-public final class LoggingAdvice {
-	
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+
+	Logger log = LoggerFactory.getLogger("Logging Aspect...");
+
+	@Pointcut("execution(* cz.boris.hibernate.repository.*Repository+.*(..))")
+	public void loggingAspect() {
+	}
+
+	@Around("loggingAspect()")
+	public Object log(ProceedingJoinPoint pjp) {
+		StopWatch sw = new StopWatch(pjp.getTarget().getClass().getName());
+		sw.start(pjp.getSignature().getName());
+		try {
+			Object o = pjp.proceed();
+			sw.stop();
+			log.info(sw.prettyPrint());
+			return o;
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
